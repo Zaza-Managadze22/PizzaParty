bde873b45caeaa357fb881c7d380673f1deeb607
Zaza Managadze <zmanagadze@sdsu.edu>
2024-02-02 13:43:33 +0400

    Pizza Party App

---
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.zybooks.pizzaparty
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Main activity for the Pizza Party app.
+ */
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var numAttendEditText: EditText
+    private lateinit var numPizzasTextView: TextView
+    private lateinit var howHungryRadioGroup: RadioGroup
+
+    /**
+     * Initializes the activity and sets up the UI components.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        numAttendEditText = findViewById(R.id.num_attend_edit_text)
+        numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
+        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+    }
+
+    /**
+     * Handles the button click event to calculate the number of pizzas needed.
+     * @param view The clicked view.
+     */
+    fun calculateClick(view: View) {
+        // Get the text that was typed into the EditText
+        val numAttendStr = numAttendEditText.text.toString()
+
+        // Convert the text into an integer
+        val numAttend = numAttendStr.toIntOrNull() ?: 0
+
+        // Get hunger level selection
+        val hungerLevel = when (howHungryRadioGroup.checkedRadioButtonId) {
+            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
+            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
+            else -> PizzaCalculator.HungerLevel.RAVENOUS
+        }
+
+        // Get the number of pizzas needed
+        val calc = PizzaCalculator(numAttend, hungerLevel)
+        val totalPizzas = calc.totalPizzas
+
+        // Place totalPizzas into the string resource and display
+        val totalText = getString(R.string.total_pizzas_num, totalPizzas)
+        numPizzasTextView.text = totalText
+    }
+}
---
+++ app/src/main/java/com/zybooks/pizzaparty/PizzaCalculator.kt
@@ -0,0 +1,46 @@
+package com.zybooks.pizzaparty
+
+import kotlin.math.ceil
+
+/**
+ * A class that calculates the number of pizzas needed for a pizza party based on the party size and hunger level.
+ *
+ * @property partySize The size of the party, representing the number of people.
+ * @property hungerLevel The hunger level of the party, influencing the number of slices each person consumes.
+ */
+class PizzaCalculator(partySize: Int, private var hungerLevel: HungerLevel) {
+    // Ensuring that partySize is non-negative
+    private var partySize = 0
+        set(value) {
+            field = if (value >= 0) value else 0
+        }
+
+    /**
+     * Enumeration representing different hunger levels with corresponding number of slices each person consumes.
+     *
+     * @property numSlices Number of slices each person consumes based on the hunger level.
+     */
+    enum class HungerLevel(var numSlices: Int) {
+        LIGHT(2), MEDIUM(3), RAVENOUS(4)
+    }
+
+    /**
+     * Calculates the total number of pizzas needed for the party based on party size and hunger level.
+     *
+     * @return The total number of pizzas required for the party.
+     */
+    val totalPizzas: Int
+        get() {
+            return ceil(partySize * hungerLevel.numSlices / SLICES_PER_PIZZA.toDouble()).toInt()
+        }
+
+    //  Initializes the PizzaCalculator with the given party size.
+    init {
+        this.partySize = partySize
+    }
+
+    companion object {
+        // Constant representing the number of slices per pizza
+        private const val SLICES_PER_PIZZA = 8
+    }
+}
